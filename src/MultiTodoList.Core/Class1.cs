03341912fd55c5f94f3b10ca9692346aa68afb1b
Victor <victor@mail.ru>
2019-05-12 04:24:16 +0300

    Added Color object

--- src/MultiTodoList.Core/Class1.cs
+++ src/MultiTodoList.Core/Class1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text.RegularExpressions;
 
 namespace MultiTodoList.Core
@@ -34,6 +35,48 @@ namespace MultiTodoList.Core
             Value = value;
         }
     }
+
+    public enum ColorField
+    {
+        Red,
+        Green,
+        Blue
+    }
+
+    public class ColorValueToBigException : Exception
+    {
+        public ColorField Field { get; }
+        public uint Value { get; }
+
+        public ColorValueToBigException(ColorField field, uint value)
+        {
+            Field = field;
+            Value = value;
+        }
+    }
+
+    public struct Color
+    {
+        public uint Red { get; }
+        public uint Green { get; }
+        public uint Blue { get; }
+
+        public Color(uint red, uint green, uint blue)
+        {
+            if (red > 255)
+                throw new ColorValueToBigException(ColorField.Red, red);
+            
+            if (green > 255)
+                throw new ColorValueToBigException(ColorField.Green, green);
+            
+            if (blue > 255)
+                throw new ColorValueToBigException(ColorField.Blue, blue);
+            
+            Red = red;
+            Green = green;
+            Blue = blue;
+        }
+    }
     
     public class Todo
     {
@@ -49,6 +92,7 @@ namespace MultiTodoList.Core
         
         public Guid Id { get;}
         public Name Name { get; }
+        public Color Color { get; }
        
     }
     public class User
