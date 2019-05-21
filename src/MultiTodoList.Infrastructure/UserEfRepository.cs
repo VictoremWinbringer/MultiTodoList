633a04e2bfdf6e2a3abfbd8f3d7f9b9391eacf85
Victor <victor@mail.ru>
2019-05-22 00:22:04 +0300

    added db context

--- src/MultiTodoList.Infrastructure/UserEfRepository.cs
+++ src/MultiTodoList.Infrastructure/UserEfRepository.cs
@@ -11,38 +11,77 @@ using Remotion.Linq.Clauses;
 
 namespace MultiTodoList.Infrastructure
 {
+    
     [Table("Users")]
     class UserDbDto
     {
         [Key] public Guid Id { get; set; }
         public byte[] Photo { get; set; }
-        public int Age { get; set; }
+        public uint Age { get; set; }
         public string Name { get; set; }
+        
+        public List<TodoGroupDbDto> Groups { get; set; }
+
+        public User To()
+        {
+            return new User(Id,new Photo(Photo),new Age(Age),new Name(Name), Groups.Select(g=>g.To()).ToList());
+        }
+
+        public static UserDbDto From(User user)
+        {
+            return new UserDbDto
+            {
+                Id = user.Id,
+                Age = user.Age.Value,
+                Name = user.Name.Value,
+                Photo = user.Photo.Value,
+                Groups = user.Groups.Select(TodoGroupDbDto.From).ToList()
+            };
+        }
     }
 
     [Table("Todos")]
-    class TodoDbDto
+   public class TodoDbDto
     {
-        [Key] public string Name { get; }
+        [Key] public string Name { get; set; }
         public bool IsComplited { get; set; }
         public DateTime Created { get; set; }
         public DateTime Complited { get; set; }
-        public UserDbDto User { get; set; }
-        public TodoGroupDbDto TodoGroup { get; set; }
+        
+        public Todo To()
+        {
+            return new Todo(new Name(Name),IsComplited, Created, Complited);
+        }
+
+        public static TodoDbDto From(Todo todo)
+        {
+            return new TodoDbDto
+            {
+                Name = todo.Name.Value,
+                IsComplited = todo.IsComplited,
+                Created = todo.Created,
+                Complited = todo.Complited,
+            };
+        }
     }
+
+   public class MultiTodoListDbContext : DbContext
+   {
+       public DbSet<UserDbDto> Users { get; set; }
+   }
 
     [Table("TodoGroups")]
     class TodoGroupDbDto
     {
         [Key] public string Name { get; set; }
-
         public byte Red { get; set; }
         public byte Green { get; set; }
         public byte Blue { get; set; }
+        public List<TodoDbDto> Todos { get; set; }
 
-        public TodoGroup To(List<Todo> todos)
+        public TodoGroup To()
         {
-            return new TodoGroup(new Name(Name), new Color(Red, Green, Blue), todos);
+            return new TodoGroup(new Name(Name), new Color(Red, Green, Blue), Todos.Select(t=>t.To()).ToList());
         }
 
         public static TodoGroupDbDto From(TodoGroup group)
@@ -52,23 +91,25 @@ namespace MultiTodoList.Infrastructure
                 Name = group.Name.Value,
                 Blue = group.Color.Blue,
                 Green = group.Color.Green,
-                Red = group.Color.Red
+                Red = group.Color.Red,
+                Todos = group.Todos.Select(TodoDbDto.From).ToList()
             };
         }
     }
 
     public class UserEfRepository : IUserRepository
     {
-        private readonly DbSet<User> _users;
+        private readonly MultiTodoListDbContext _usersContext;
 
-        public UserEfRepository(DbSet<User> users)
+        public UserEfRepository(MultiTodoListDbContext usersContext)
         {
-            _users = users;
+            _usersContext = usersContext;
         }
 
         public void Create(User user)
         {
-            _users.Add(user);
+            _usersContext.Users.Add(UserDbDto.From(user));
+            _usersContext.SaveChanges();
         }
 
         public User Get(Guid id)
